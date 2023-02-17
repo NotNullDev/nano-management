11c6243536b050a7812fee3371b4e31ed061e21e
notnulldev <notnulldev0@gmail.com>
2023-02-17 22:47:13 +0100

    updated build scripts

--- .nano-ci-cd/pre-build.sh
+++ .nano-ci-cd/pre-build.sh
@@ -1,1 +1,12 @@
+mkdir /root/.ssh/
+
+cat <<eof > /test
+echo $SSH_PRIVATE_KEY | base64 -d > /root/.ssh/id_rsa
+eof
+
+chmod 400 /root/.ssh/id_rsa
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+git submodule update --recursive --remote
+
 echo $CONTAINER_REGISTRY_PASSWORD | docker login -u $CONTAINER_REGISTRY_USERNAME --password-stdin $CONTAINER_REGISTRY_URL
